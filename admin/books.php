fe11ff552b3d2173bf00e7dab389c696e948bf88
J19024 <j19024@sangi.jp>
2020-07-02 15:07:44 +0900

    first commit

---
+++ admin/books.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<body>
+<h2>Practice</h2>
+    <pre>
+        <?php
+            try{
+                $db = new PDO('myspl;dbname=books;host=127.0.0.1:charset=utf8','root');
+            }
+        ?>
+    </pre>
