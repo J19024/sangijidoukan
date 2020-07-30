a0638a6f24aaeb9695c450b98633f4748ebfe3a2
hibiking <hibi0422@gmail.com>
2020-07-30 15:24:11 +0900

    一覧デザイン

--- test/test02.php
+++ test/test02.php
@@ -1,34 +1,5 @@
 <?php
-// エラーを出力させない
-ini_set('display_errors', "Off");
-require('../dbconnect.php');
-session_start();
-
-if (!empty($_POST)) {
-  $TEST = "テスト";
-	// ログインの処理
-	if ($_POST['user_id'] != '' && $_POST['password'] != '') {
-    $login = $db->prepare('SELECT * FROM users WHERE user_id=? AND password=?');
-    $login->execute(array(
-      $_POST['user_id'],
-      //sha1($_POST['password'])
-      $_POST['password']
-    ));
-    $member = $login->fetch();
-
-    if ($member) {
-			// ログイン成功
-			$_SESSION['user_id'] = $member['user_id'];
-      $_SESSION['time'] = time();
-      header('Location: books.php');
-        exit();
-		} else {
-			@$error['login'] = 'failed';
-		}
-	} else {
-		@$error['login'] = 'blank';
-	  }
-  }
+    echo $_POST['user_id'];
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -44,17 +15,10 @@ if (!empty($_POST)) {
 <body>
   <div id="app">
     <v-app>
-    <?php if ($error['login'] == 'blank'): ?>
-            <p class="error">* メールアドレスとパスワードをご記入ください</p>
-            <?php endif; ?>
-            <?php if ($error['login'] == 'failed'): ?>
-            <p class="error">* ログインに失敗しました。正しくご記入ください。</p>
-    <?php endif; ?>
       <v-card width="400px" class="mx-auto mt-5">
         <v-card-title>
           <h1 class="display-1">ログイン</h1>
         </v-card-title>
-        
         <v-card-text>
         <v-form v-model="valid" ref='form' action="" method="post">
           <v-container>
@@ -83,14 +47,19 @@ if (!empty($_POST)) {
                     required
                   ></v-text-field>
             </v-container>
-            <v-card-actions>
-              <v-btn class="info" type="submit">ログイン</v-btn>
-            </v-card-actions>
+            <v-btn class="info" type="submit">ログイン</v-btn>
           </v-form>
+          <form action="" method="post">
+            <input type="hidden" name="user_id" size="35" maxlength="255" value="test" />
+            <input type="hidden" name="password" size="35" maxlength="255" value="12345" />
+            <v-card-actions>
+            </v-card-actions>
+          </form>
         </v-card-text>
       </v-card>
     </v-app>
   </div>
+
 
 <script src="https://cdn.jsdelivr.net/npm/vue@2.x/dist/vue.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js"></script>
